e809a0b09eaebcd3d08328c0ea7a11777d17cbb2
Alex Adam <alex@eg312.com>
2019-04-03 14:56:01 +0300

    misc updates

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 arch
 ====
 
-archives & encrypts a file/folder
+Archives & encrypts a file/folder
 
 ./arch.sh input_path
 
 Options:
 
-ef : (if input_path is directory -> tar) ->  encript file 
+e : encrypt file or directory
 
-df : decrypt input_path
-
-em : for each file in input_path -> (if file is directory -> tar) -> encrypt
-
-dm : for each file in input_path -> decrypt
-
+d : decrypt input_path
--- arch.sh
+++ arch.sh
@@ -4,26 +4,31 @@ appendTimeStamp=1	#append time stamp to file
 removeArch=0	#remove the .tar file after it has been encrypted
 inputPath=${1%/} #removing trailing "/"
 
-function eff {
+function encryptFilesOrDirectories {
 	archName=$1
 	local archFileName=${1##*/} # get the file name from path
-	
+
+	if [ $appendTimeStamp == 1 ]; then
+		archName="$archFileName$now.tar"
+	else
+		archName="$archFileName.tar"
+	fi
+
 	if [ -d $1 ]; then
-		if [ $appendTimeStamp == 1 ]; then
-			archName="$archFileName$now.tar"
-		else
-			archName="$archFileName.tar"
-		fi
-		
-		# change tar's dir to input directorys's parent path - do not add the parents to the archive 
+		# it's a directory
+		# change tar's dir to input directorys's parent path - do not add the parents to the archive
 		# -p for preserving file permissions
 		tar -C $(dirname "$1") -cpf $archName $archFileName
 		removeArch=1
+	elif [ -f $1 ]; then
+		# it's a file
+		tar -cpf $archName $archFileName
+		removeArch=1
 	fi
-	
+
 	echo $2 | gpg --batch -q --passphrase-fd 0 --cipher-algo AES256 -c $archName
-	
-	if [ $removeArch == 1 ]; then	
+
+	if [ $removeArch == 1 ]; then
 		rm $archName
 	fi
 }
@@ -47,44 +52,42 @@ if [ $tmpPass1 != $tmpPass2 ]; then
 	exit
 fi
 
-echo "Select mode: [e|d]f = [en|de]crypt file OR [e|d]m = [en|de]crypt each file in folder"
+# echo "Select mode: [e|d]f = [en|de]crypt file OR [e|d]m = [en|de]crypt each file in folder"
+echo "Select mode:"
+echo "e = encrypt file OR directory"
+echo "d = decrypt file"
 read mode
 
 now=$(date +"-%Y-%m-%d-%H-%M")
 
-if [ $mode == "ef" ]; then
-	eff $inputPath $tmpPass1
-elif [ $mode == "df" ]; then
+if [ $mode == "e" ]; then
+	encryptFilesOrDirectories $inputPath $tmpPass1
+elif [ $mode == "d" ]; then
 	dff $inputPath $tmpPass1 ${inputPath%.*}
-elif [ $mode == "em" ] && [ -d $inputPath ]; then
-	appendTimeStamp=0
-	tmpFolderName="enc_tmp_folder$now"
-	mkdir $tmpFolderName
-	
-	for file in $inputPath/*
-	do
-		eff $file $tmpPass1
-		mv "$archName.gpg" $tmpFolderName
-	done
-elif [ $mode == "dm" ] && [ -d $inputPath ]; then
-	tmpFolderName="dec_tmp_folder$now"
-	mkdir $tmpFolderName
-	
-	for file in $inputPath/*
-	do
-		fileName=${file##*/}
-		outputFileName=${fileName%.*}
-		
-		dff $file $tmpPass1 "$tmpFolderName/$outputFileName"
-	done
+#### encrypt / decrypt each folder in directory
+# elif [ $mode == "em" ] && [ -d $inputPath ]; then
+# 	appendTimeStamp=0
+# 	tmpFolderName="enc_tmp_folder$now"
+# 	mkdir $tmpFolderName
+#
+# 	for file in $inputPath/*
+# 	do
+# 		encryptFilesOrDirectories $file $tmpPass1
+# 		mv "$archName.gpg" $tmpFolderName
+# 	done
+# elif [ $mode == "dm" ] && [ -d $inputPath ]; then
+# 	tmpFolderName="dec_tmp_folder$now"
+# 	mkdir $tmpFolderName
+#
+# 	for file in $inputPath/*
+# 	do
+# 		fileName=${file##*/}
+# 		outputFileName=${fileName%.*}
+#
+# 		dff $file $tmpPass1 "$tmpFolderName/$outputFileName"
+# 	done
+##########
 else
 	echo "Invalid parameters!"
 	exit
 fi
-
-
-#archName=$1$now".tar"
-#echo $archName
-#tar -cf $archName $1
-#gpg --cipher-algo AES256 -c $archName
-#rm $archName
